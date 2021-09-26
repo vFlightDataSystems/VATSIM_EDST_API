2b2b8d8ad409f7fd20434de51aab2007be1f0db8
Tux <34196765+CaptainTux@users.noreply.github.com>
2021-09-27 00:38:10 +0200

    new routes, export amend function

--- blueprints/flightplans_bp.py
+++ blueprints/flightplans_bp.py
@@ -1,25 +1,32 @@
-from flask import Blueprint, jsonify, g
+from flask import Blueprint, jsonify
 
-from lib.adr_lib import get_best_adr, amend_adr
-from lib.lib import get_flightplan, expand_route, get_faa_prd, get_adar
+import lib.lib
 
 flightplans_blueprint = Blueprint('flightplan', __name__)
 
 
-@flightplans_blueprint.route('<callsign>')
+@flightplans_blueprint.route('all')
+def _get_all_flightplans():
+    return jsonify(lib.lib.get_all_flightplans())
+
+
+@flightplans_blueprint.route('callsign/<callsign>')
 def _get_flightplan(callsign):
-    return jsonify(get_flightplan(callsign))
+    if fp := lib.lib.get_flightplan(callsign):
+        return jsonify(fp)
+    else:
+        return 404, ''
 
 
-@flightplans_blueprint.route('amendments/<callsign>')
+@flightplans_blueprint.route('amendments/all')
+def _get_all_amended_flightplans():
+    return jsonify(lib.lib.get_all_amended_flightplans())
+
+
+@flightplans_blueprint.route('amendments/callsign/<callsign>')
 def _get_amended_flightplan(callsign):
-    if fp := get_flightplan(callsign):
-        if fp.departure and fp.route:
-            split_route = fp.route.split()
-            adr_list = get_best_adr(fp.departure, split_route, int(fp.altitude), fp.aircraft_short)
-            adar_list = get_adar(fp.departure, fp.arrival)
-            fp.amendments = dict()
-            fp.amendments['adr'] = [amend_adr(split_route, expand_route(split_route), adr) for adr in adr_list]
-            fp.amendments['adar'] = adar_list
-            fp.amendments['faa_prd'] = get_faa_prd(fp.departure, fp.arrival)
-    return jsonify(fp)
+    if fp := lib.lib.get_flightplan(callsign):
+        fp = lib.lib.amend_flightplan(fp)
+        return jsonify(fp)
+    else:
+        return 404, ''
