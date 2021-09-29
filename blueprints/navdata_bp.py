5be3a2833fc3ee8fde4b19a1d4246763e97a1618
Tux <34196765+CaptainTux@users.noreply.github.com>
2021-09-29 23:48:57 +0200

    add `{'_id': false}` in queries, add route for procedures given an airport

--- blueprints/navdata_bp.py
+++ blueprints/navdata_bp.py
@@ -6,26 +6,37 @@ navdata_blueprint = Blueprint('navdata', __name__)
 @navdata_blueprint.route('/airport/<airport>')
 def _get_airport(airport):
     client = g.mongo_nav_client
-    airport_data = next(client.navdata.airports.find({'icao': airport}), None)
+    airport_data = client.navdata.airports.find_one({'icao': airport}, {'_id': False})
     return jsonify(airport_data)
+
+
+@navdata_blueprint.route('/airport/<airport>/procedures')
+def _get_airport_procedures(airport):
+    client = g.mongo_nav_client
+    data = list(client.navdata.procedures.find({'airports': {'$elemMatch': {'airport': airport}}}, {'_id': False}))
+    for row in data:
+        airports = row['airports']
+        row['runways'] = airports[0]['runways']
+        del row['airports']
+    return jsonify(data)
 
 
 @navdata_blueprint.route('/airway/<airway>')
 def _get_airway(airway):
     client = g.mongo_nav_client
-    airway_data = next(client.navdata.airways.find({'airway': airway}), None)
+    airway_data = list(client.navdata.airways.find({'airway': airway}, {'_id': False}))
     return jsonify(airway_data)
 
 
 @navdata_blueprint.route('/waypoint/<waypoint>')
 def _get_waypoint(waypoint):
     client = g.mongo_nav_client
-    waypoint_data = next(client.navdata.waypoints.find({'waypoint_id': waypoint}), None)
+    waypoint_data = list(client.navdata.waypoints.find({'waypoint_id': waypoint}, {'_id': False}))
     return jsonify(waypoint_data)
 
 
 @navdata_blueprint.route('/procedure/<procedure>')
 def _get_procedure(procedure):
     client = g.mongo_nav_client
-    procedure_data = next(client.navdata.procedures.find({'procedure': procedure}), None)
+    procedure_data = client.navdata.procedures.find_one({'procedure': procedure}, {'_id': False})
     return jsonify(procedure_data)
