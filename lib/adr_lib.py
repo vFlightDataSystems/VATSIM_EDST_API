f8bbeebc52281171e0e61db71db4da5f4cb70698
Tux <34196765+CaptainTux@users.noreply.github.com>
2021-10-31 19:35:02 +0100

    minor fixes

--- lib/adr_lib.py
+++ lib/adr_lib.py
@@ -26,44 +26,45 @@ def amend_adr(route: str, adr: dict) -> dict:
     :param adr: adr dictionary as it is returned from the database
     :return: dictionary containing: the adr upto tfix, rest of the route starting after the tfix, route groups for the adr
     """
+    split_route = route.split()
     adr_route = adr['route']
     # if adr matches initial route, there is nothing to do.
     if adr_route == route[:len(adr_route)]:
         adr_route = ''
     else:
-        expanded_adr = lib.lib.expand_route(adr_route, airways=adr['airways'])
+        expanded_adr = lib.lib.expand_route(adr_route, airways=adr['airways']).split()
+        expanded_route = lib.lib.expand_route(route).split()
         tfix_list = adr['tfixes']
         tfixes = [e['tfix'] for e in tfix_list]
         info_dict = {e['tfix']: e['info'] for e in tfix_list}
-        expanded_route = lib.lib.expand_route(route).split()
         for fix in reversed(expanded_route):
             # find farthest tfix which triggered the ADR
             if fix in tfixes:
                 info = info_dict[fix]
-                if fix in route and 'Explicit' in info:
-                    index = route.index(fix)
+                if fix in split_route and 'Explicit' in info:
+                    index = split_route.index(fix)
                     adr_route = slice_adr(adr_route, fix)
                     if adr_route != route[:len(adr_route)]:
-                        route = route[index:]
+                        route = ' '.join(split_route[index:])
                     else:
                         adr_route = ''
                     break
                 elif 'Implicit' in info:
                     index = expanded_route.index(fix)
                     route_fix = [e for e in expanded_adr[index:] if e in route][-1]
-                    route_index = route.index(route_fix)
+                    route_index = split_route.index(route_fix)
                     adr_route = slice_adr(adr_route, fix)
                     if adr_route != route[:len(adr_route)]:
-                        route = route[route_index:]
+                        route = ' '.join(split_route[route_index:])
                     else:
                         adr_route = []
                     break
                 elif info == 'Append':
                     index = expanded_route.index(fix)
-                    route_fix = [e for e in expanded_route[index:] if e in route][0]
-                    route_index = route.index(route_fix)
+                    route_fix = [e for e in expanded_route[index:] if e in split_route][0]
+                    route_index = split_route.index(route_fix)
                     if adr_route != route[:len(adr_route)]:
-                        route = route[route_index:]
+                        route = ' '.join(split_route[route_index:])
                     else:
                         adr_route = []
                     break
@@ -85,12 +86,12 @@ def get_eligible_adr(fp) -> list:
          "aircraft_class": {"$elemMatch": {"$in": nat_list}}
          }, {'_id': False}))
     eligible_adr = []
-    expanded_route = lib.lib.expand_route(fp.route)
+    expanded_route = lib.lib.expand_route(fp.route).split()
     for adr in adr_list:
         if (int(adr['min_alt']) <= filed_alt <= int(adr['top_alt'])) or filed_alt == 0:
             for tfix in adr['tfixes']:
                 if (('Explicit' in tfix['info'] and
-                     tfix['tfix'] in fp.route) or
+                     tfix['tfix'] in fp.route.split()) or
                         ('Implicit' in tfix['info'] and
                          tfix['tfix'] in expanded_route) or
                         (tfix['tfix'] in expanded_route and
