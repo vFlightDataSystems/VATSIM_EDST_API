bc6a6b4c22922da33b40d94fb639728545c1e855
Tux <34196765+CaptainTux@users.noreply.github.com>
2021-11-28 01:56:22 +0100

    stay consistent!

--- resources/AdaptationProfile.py
+++ resources/AdaptationProfile.py
@@ -3,10 +3,10 @@ class AdaptationProfile:
     def __init__(self, profile=None):
         self.climbout_options = profile['climbout_options'] if 'climbout_options' in profile.keys() else []
         self.climbvia_options = profile['climbvia_options'] if 'climbvia_options' in profile.keys() else []
-        self.initial_altitude_options = profile['initial_altitude_options']\
-            if 'initial_altitude_options' in profile.keys() else []
-        self.expect_cruise_options = profile['expect_cruise_options'] \
-            if 'expect_cruise_options' in profile.keys() else []
+        self.initial_altitude_options = profile['initial_alt_options']\
+            if 'initial_alt_options' in profile.keys() else []
+        self.expect_cruise_options = profile['expect_options'] \
+            if 'expect_options' in profile.keys() else []
         self.dep_freq_options = profile['dep_freq_options'] if 'dep_freq_options' in profile.keys() else []
         self.facility = profile['facility'] if 'facility' in profile.keys() else ''
         self.profile_name = profile['profile_name'] if 'profile_name' in profile.keys() else ''
