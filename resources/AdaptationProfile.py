e28ba4d9bc9b487c8088d2c37f242089234868d2
Tux <34196765+CaptainTux@users.noreply.github.com>
2021-11-28 02:01:05 +0100

    forgot something

--- resources/AdaptationProfile.py
+++ resources/AdaptationProfile.py
@@ -8,6 +8,8 @@ class AdaptationProfile:
         self.expect_cruise_options = profile['expect_options'] \
             if 'expect_options' in profile.keys() else []
         self.dep_freq_options = profile['dep_freq_options'] if 'dep_freq_options' in profile.keys() else []
+        self.contact_info_options = profile['contact_info_options'] if 'contact_info_options' in profile.keys() else []
+        self.local_info_options = profile['local_info_options'] if 'local_info_options' in profile.keys() else []
         self.facility = profile['facility'] if 'facility' in profile.keys() else ''
         self.profile_name = profile['profile_name'] if 'profile_name' in profile.keys() else ''
         self.username = profile['username'] if 'username' in profile.keys() else ''
