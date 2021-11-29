f7b4829345587885aac44e3faafbdb3e8b688763
Tux <34196765+CaptainTux@users.noreply.github.com>
2021-11-29 17:02:04 +0100

    more type checking

--- blueprints/navdata_bp.py
+++ blueprints/navdata_bp.py
@@ -4,16 +4,16 @@ navdata_blueprint = Blueprint('navdata', __name__)
 
 
 @navdata_blueprint.route('/airport/<airport>')
-def _get_airport(airport):
+def _get_airport(airport: str):
     client = g.mongo_nav_client
-    airport_data = client.navdata.airports.find_one({'icao': airport}, {'_id': False})
+    airport_data = client.navdata.airports.find_one({'icao': airport.upper()}, {'_id': False})
     return jsonify(airport_data)
 
 
 @navdata_blueprint.route('/airport/<airport>/procedures')
-def _get_airport_procedures(airport):
+def _get_airport_procedures(airport: str):
     client = g.mongo_nav_client
-    data = list(client.navdata.procedures.find({'airports': {'$elemMatch': {'airport': airport}}}, {'_id': False}))
+    data = list(client.navdata.procedures.find({'airports': {'$elemMatch': {'airport': airport.upper()}}}, {'_id': False}))
     for row in data:
         airports = row['airports']
         row['runways'] = airports[0]['runways']
@@ -22,21 +22,21 @@ def _get_airport_procedures(airport):
 
 
 @navdata_blueprint.route('/airway/<airway>')
-def _get_airway(airway):
+def _get_airway(airway: str):
     client = g.mongo_nav_client
-    airway_data = list(client.navdata.airways.find({'airway': airway}, {'_id': False}))
+    airway_data = list(client.navdata.airways.find({'airway': airway.upper()}, {'_id': False}))
     return jsonify(airway_data)
 
 
 @navdata_blueprint.route('/waypoint/<waypoint>')
-def _get_waypoint(waypoint):
+def _get_waypoint(waypoint: str):
     client = g.mongo_nav_client
-    waypoint_data = list(client.navdata.waypoints.find({'waypoint_id': waypoint}, {'_id': False}))
+    waypoint_data = list(client.navdata.waypoints.find({'waypoint_id': waypoint.upper()}, {'_id': False}))
     return jsonify(waypoint_data)
 
 
 @navdata_blueprint.route('/procedure/<procedure>')
-def _get_procedure(procedure):
+def _get_procedure(procedure: str):
     client = g.mongo_nav_client
-    procedure_data = client.navdata.procedures.find_one({'procedure': procedure}, {'_id': False})
+    procedure_data = client.navdata.procedures.find_one({'procedure': procedure.upper()}, {'_id': False})
     return jsonify(procedure_data)
