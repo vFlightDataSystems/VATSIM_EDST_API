cba913d3a0be4c00395af822f0d3f5725b07c066
Tux <34196765+CaptainTux@users.noreply.github.com>
2021-12-02 23:46:36 +0100

    add weather route

---
+++ blueprints/weather_bp.py
@@ -0,0 +1,17 @@
+import requests
+from lxml import etree
+from flask import Blueprint, jsonify
+
+weather_blueprint = Blueprint('weather', __name__)
+
+
+@weather_blueprint.route('/metar/airport/<airport>')
+def _metar(airport):
+    response = requests.get(
+        f'https://www.aviationweather.gov/adds/dataserver_current'
+        f'/httpparam?dataSource=metars&requestType=retrieve&format=xml&stationString={airport}&hoursBeforeNow=2')
+
+    tree = etree.fromstring(response.content)
+    metar_list = [e.text for e in tree.iter('raw_text')]
+
+    return jsonify(metar_list)
