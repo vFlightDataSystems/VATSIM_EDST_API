399fef36754109667ff76cfd01c246539a6ff14c
Tux <34196765+CaptainTux@users.noreply.github.com>
2021-12-05 20:20:13 +0100

    mongo changes

--- blueprints/adr_bp.py
+++ blueprints/adr_bp.py
@@ -1,12 +1,18 @@
-from flask import Blueprint
+import json
+
+from bson import json_util
+from flask import Blueprint, g, jsonify
+from pymongo import MongoClient
 
 adr_blueprint = Blueprint('adr', __name__)
 
 
-@adr_blueprint.route('/', methods=['POST'])
-def _get_adr():
+@adr_blueprint.route('/get/artcc/<artcc>', methods=['GET'])
+def _get_adr(artcc: str):
     """
 
     :return:
     """
-    return '', 204
+    client: MongoClient = g.mongo_reader_client
+    adr_list = client[artcc].adr.find({})
+    return jsonify(json.loads(json_util.dumps(adr_list)))
