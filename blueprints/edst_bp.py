034fa2b205f034ecc639315a9099fdcdbd56df2a
Tux <34196765+CaptainTux@users.noreply.github.com>
2022-05-13 16:52:14 +0200

    error handling

--- blueprints/edst_bp.py
+++ blueprints/edst_bp.py
@@ -44,23 +44,24 @@ def _update_entry():
 @edst_blueprint.route('/trial/route', methods=['POST'])
 def _trial_route_amendment():
     post_data = request.get_json()
-    print(post_data)
+    keys = post_data.keys()
     amend_data = libs.edst_lib.get_amended_route(route=post_data['route'],
                                                  route_data=post_data['route_data'],
-                                                 direct_fix=post_data['direct_fix'],
-                                                 frd=post_data['frd'],
-                                                 dest=post_data['dest'])
+                                                 direct_fix=post_data['direct_fix'] if 'direct_fix' in keys else None,
+                                                 frd=post_data['frd'] if 'frd' in keys else None,
+                                                 dest=post_data['dest']) if 'dest' in keys else None
     return jsonify(amend_data) if amend_data else jsonify(204)
 
 
 @edst_blueprint.route('/entry/amend/route', methods=['POST'])
 def _amend_route():
     post_data = defaultdict(request.get_json())
+    keys = post_data.keys()
     amend_data = libs.edst_lib.get_amended_route(route=post_data['route'],
                                                  route_data=post_data['route_data'],
-                                                 direct_fix=post_data['direct_fix'],
-                                                 frd=post_data['frd'],
-                                                 dest=post_data['dest'])
+                                                 direct_fix=post_data['direct_fix'] if 'direct_fix' in keys else None,
+                                                 frd=post_data['frd'] if 'frd' in keys else None,
+                                                 dest=post_data['dest']) if 'dest' in keys else None
     if amend_data and post_data['callsign']:
         libs.edst_lib.update_edst_entry(post_data['callsign'], amend_data)
     return jsonify(amend_data) if amend_data else jsonify(204)
